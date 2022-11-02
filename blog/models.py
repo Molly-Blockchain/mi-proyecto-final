f08783e763abd65e4f5e0544505a134bfd3cba78
Molly-Blockchain <molly.blockchain@gmail.com>
2022-11-02 20:33:58 -0300

    guardo ultimos cambios antes de mas modificaciones

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Configuracion(models.Model):
+    nombre_blog = models.CharField(max_length = 14, default = '')
+    construido_por = models.CharField(max_length = 30, default = '')
+    titulo_principal = models.CharField(max_length = 30, default = '')
+    subtitulo_principal = models.CharField(max_length = 30, default = '')
+
